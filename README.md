c693119f4bb35d9921e54fe7a5b853341a6bbf05
unknown <90862670+Vadim-kaR@users.noreply.github.com>
2022-09-26 13:57:34 +0300

    add CLIApp

---
+++ README.md
@@ -0,0 +1,7 @@
+1.list - https://monosnap.com/file/Gg87bQPGkeeNCzZDLUkhE1xWP9yxAc
+
+2.getById - https://monosnap.com/file/dQRSu5DzjGvQxUiKLdfCzW4sNi6cVW
+
+3.add - https://monosnap.com/file/u5eOdArFIZ6eHlF1uEj7wKEOnAs85b
+
+4.remove - https://monosnap.com/file/jvomoHX2lcU58CZaF5JZU764VWn0Zb
---
+++ contacs.js
@@ -0,0 +1,53 @@
+const { nanoid } = require("nanoid");
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const updateContacts = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+};
+
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((item) => item.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = allContacts.splice(index, 1);
+  updateContacts(allContacts);
+  return result;
+};
+
+const addContact = async (data) => {
+  const contacts = await listContacts();
+
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
